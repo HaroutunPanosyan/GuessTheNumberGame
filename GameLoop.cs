912c0527a9a717ae2a33bcbec5131f43a87508bb
HaroutunPanosyan <harupanosyan@gmail.com>
2024-03-06 13:44:30 -0800

    Initial commit

---
+++ GameLoop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public class GameLoop
+    {
+        public GameLoop() 
+        {
+            int cpuNumber = new Random().Next(1, 101);
+
+            bool validInput = false;
+            int guess;
+            do
+            {
+                Console.WriteLine(Prompts.InitialPrompt());
+                string? guessString = Console.ReadLine();
+                validInput = int.TryParse(guessString, out guess);
+                if (!validInput)
+                {
+                    Console.WriteLine(Prompts.Error());
+                }
+            } while (!validInput);    
+
+            while (guess != cpuNumber)
+            {
+                    Console.WriteLine(Prompts.Loss(guess, cpuNumber));
+                    guess = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(Prompts.Win(guess, cpuNumber));
+
+            Console.WriteLine(Prompts.PlayAgain());
+            string? response = Console.ReadLine();
+            switch (response?.ToLower())
+            {
+                case "y":
+                    GameLoop newGame = new GameLoop();
+                    break;
+                case "yes":
+                    newGame = new GameLoop();
+                    break;
+                case "yea":
+                    newGame = new GameLoop();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Prompts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    public static class Prompts
+    {
+        public static string InitialPrompt()
+        {
+            return "Guess a number between 1 and 100:   ";
+        }
+        public static string Win(int guess, int cpuNumber)
+        {
+            return "You guessed it! Congrats!" ;
+        }
+        public static string Loss(int guess, int cpuNumber)
+        {
+            if (guess < cpuNumber)
+            {
+                return "Guess is too low, try again.";
+            }
+            else
+            {
+                return "Guess is too high, try again.";
+            }
+        }
+        public static string PlayAgain()
+        {
+            return "Play again? Y, or Yes to start again.";
+        }
+        public static string Error()
+        {
+            return "Not a valid input.";
+        }
+    }
+}
